bd8f6944edb85cf0a829c8f821ce39d0d37d1c9d
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-11-01 20:16:43 -0300

    feat: created sign in screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Center, Text } from 'native-base';
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor='gray.900'>
-        <Text color='white' fontSize={22}>opa</Text>
-      </Center>
+      { fontsLoaded ? <SignIn /> : <Loading /> }
       <StatusBar style="auto" />
     </NativeBaseProvider>
   );
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import { Center, Text } from 'native-base';
+
+
+export const SignIn = () => {
+  return (
+    <Center flex={1} bgColor='gray.900'>
+      <Text color='white' fontSize={22}>SignIn</Text>
+    </Center>
+  )
+}
