9fcdf2cefc1894e9702b4ec8925fe0abc592b03b
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-11-01 20:00:15 -0300

    feat: added native base theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Center, Text } from 'native-base';
+import { THEME } from './src/styles/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor='gray.900'>
+        <Text color='white' fontSize={22}>opa</Text>
+      </Center>
       <StatusBar style="auto" />
-    </View>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
