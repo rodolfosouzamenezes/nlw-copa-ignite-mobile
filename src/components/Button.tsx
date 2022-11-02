4f3f4782feb005d576155c6d484eb487c9a3d794
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-11-02 15:08:17 -0300

    feat: button component created

---
+++ src/components/Button.tsx
@@ -0,0 +1,37 @@
+import { Button as ButtonNativeBase, Text, IButtonProps } from "native-base";
+
+interface ButtonProps extends IButtonProps {
+  title: string;
+  type?: 'primary' | 'secondary';
+}
+
+export function Button({ title, type = 'primary', ...rest }: ButtonProps) {
+  return (
+    <ButtonNativeBase
+      w='full'
+      h={14}
+      rounded='sm'
+      fontSize='md'
+      textTransform='uppercase'
+      bg={type === 'secondary' ? 'red.500' : 'yellow.500'}
+      _pressed={{
+        bg: type === 'secondary' ? 'red.600' : 'yellow.600',
+      }}
+      _loading={{
+        _spinner: {
+          color: type === 'secondary' ? 'white' : 'black',
+        }
+      }}
+      {...rest}
+    >
+      <Text
+        textTransform='uppercase'
+        fontSize='sm'
+        fontFamily='heading'
+        color={type === 'secondary' ? 'white' : 'black'}
+      >
+        {title}
+      </Text>
+    </ButtonNativeBase>
+  );
+}
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,26 @@
-import { Center, Text } from 'native-base';
+import { Center, Icon, Text } from 'native-base';
 import LogoNlw from '../assets/logo.svg';
+
+import { Button } from '../components/Button';
+
+import { Fontisto } from '@expo/vector-icons'
 
 export const SignIn = () => {
   return (
-    <Center flex={1} bgColor='gray.900'>
+    <Center flex={1} bgColor='gray.900' p='7' >
       <LogoNlw width={212} height={40} />
+
+      <Button
+        title='Entrar com Google'
+        type='secondary'
+        leftIcon={<Icon as={Fontisto} name='google' color='white' size='sm' />}
+        mt='12'
+      />
+
+      <Text color='white' textAlign='center' mt='4' >
+        Não utilizamos nenhuma informação além {'\n'} 
+        do seu e-mail para criação de sua conta.
+      </Text>
     </Center>
   )
 }
