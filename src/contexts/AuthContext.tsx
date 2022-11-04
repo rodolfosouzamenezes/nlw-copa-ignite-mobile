e673cd0f8124388352eb25349cb80d2d5c2c7b7d
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-11-04 09:23:06 -0300

    feat: get user info

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,6 +3,7 @@ import * as Google from 'expo-auth-session/providers/google';
 import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from 'expo-web-browser';
 import { GOOGLE_CLIENT_ID } from '@env';
+import { api } from '../services/api';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -48,8 +49,20 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
   }
 
   async function signInWithGoogle(access_token: string) {
-    console.log('access_token ', access_token);
-    
+    try {
+      setIsUserLoading(true)
+
+      const tokenResponse = await api.post('/users', { access_token });
+      api.defaults.headers.common['Authorization'] = `Bearer ${tokenResponse.data.token}`
+      
+      const userInfoResponse = await api.get('/me');
+      setUser(userInfoResponse.data.user);     
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false)
+    }
   }
 
   useEffect(() => {
