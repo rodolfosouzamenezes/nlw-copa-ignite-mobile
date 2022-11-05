72568e98edc9066428ff24396f2bb16e9ec0441d
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-11-05 07:25:33 -0300

    feat: added date to game

--- src/components/Guesses.tsx
+++ src/components/Guesses.tsx
@@ -2,6 +2,7 @@ import { FlatList, useToast } from 'native-base';
 import { useEffect, useState } from 'react';
 import { api } from '../services/api';
 import { Game, GameProps } from './Game';
+import { Loading } from './Loading';
 
 interface Props {
   poolId: string;
@@ -35,9 +36,51 @@ export function Guesses({ poolId }: Props) {
     }
   }
 
+  async function handleGuessConfirm(gameId: string) {
+    try {
+      setIsLoading(true)
+
+      if(!firstTeamPoints.trim() || !secondTeamPoints.trim()) {
+        return toast.show({
+          title: 'Informe um placar para o palpite',
+          placement: 'top',
+          bgColor: 'red.500'
+        })
+      }
+
+      await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
+        firstTeamPoints: Number(firstTeamPoints),
+        secondTeamPoints: Number(secondTeamPoints),
+      });
+
+      toast.show({
+        title: 'Palpite realizado com sucesso',
+        placement: 'top',
+        bgColor: 'green.500'
+      })
+
+      fetchGames()
+    } catch (error) {
+      console.log(error);
+
+      toast.show({
+        title: 'Não foi possível carregar os jogos',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   useEffect(() => {
     fetchGames()
   }, [poolId])
+
+  if(isLoading) {
+    return <Loading />
+  }
 
   return (
     <FlatList 
@@ -48,7 +91,7 @@ export function Guesses({ poolId }: Props) {
           data={item}
           setFirstTeamPoints={setFirstTeamPoints}
           setSecondTeamPoints={setSecondTeamPoints}
-          onGuessConfirm={() => {}}
+          onGuessConfirm={() => handleGuessConfirm(item.id)}
         />
       )}
     />
